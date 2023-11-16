8927bf53e1a6bbe278b7635cfe354f5984e6b26d
MARTIN-339 <muirujnr@gmail.com>
2023-11-16 01:36:38 -0800

    shell

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Prints "hello world" to the standard output */
+	printf("hello world\n");
+	return (0);
+}
+
